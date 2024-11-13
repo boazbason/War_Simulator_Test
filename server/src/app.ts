f4ed9adefc6073062b657d936f9207ce587dce76
boazbasson <basonboaz@gmail.com>
2024-11-13 16:15:34 +0200

    apis for login and register, schimas, and connecting db

---
+++ server/src/app.ts
@@ -0,0 +1,35 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors"
+import connectDb from "./config/db";
+import userRouter from "./routes/userRoute";
+// import candidateRouter from "./routes/candidateRoute";
+import { StartSocket } from "./socket";
+import missiles from '../src/data/missiles'
+import { IMission, Missiles } from "./models/MissileSchema";
+import organizationsList from "./data/organizations";
+import { IOrganization, Organization } from "./models/OrganizationSchema";
+import { Users, IUser } from "./models/userSchema";
+import { log } from "console";
+
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+connectDb();
+app.use(express.json());
+app.use(cors());
+
+
+// Routes
+app.use('/api', userRouter);
+// app.use('/api/candidates', candidateRouter);
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
---
+++ server/src/controllers/userController.ts
@@ -0,0 +1,77 @@
+import {Users, IUser} from '../models/userSchema';
+import mongoose from 'mongoose';
+import { Request, Response } from 'express';
+import jwt from "jsonwebtoken";
+import { log } from 'console';
+import { IOrganization, Organization } from '../models/OrganizationSchema';
+import { IMission, Missiles } from '../models/MissileSchema';
+
+export const register = async (req: Request, res: Response) => {
+    try {
+        const { username, password, organization, location } = req.body.user;
+        if (!username || !password || !organization) {
+            res.status(200).json({ message: "Username and password are required", success: false });
+            return;
+        }
+
+        const user: IUser = {
+            username,
+            password,
+            organization,
+            resources: [],
+            location
+        };
+
+        const organizationOfUser: IOrganization | null = await Organization.findOne({ name: user.organization });
+
+        if (organizationOfUser) {
+            await Promise.all(organizationOfUser.resources.map(async (resource) => {
+                const missile: IMission | null = await Missiles.findOne({ name: resource.name });
+                if (missile) {
+                    user.resources!.push({ missile: missile._id, amount: resource.amount });
+                }
+            }));
+
+            // יצירת המשתמש לאחר שכל ה-resources התווספו
+            const UserAdded = await Users.create(user); 
+            res.status(201).json({ newUser: UserAdded, success: true });
+            return;
+        }
+
+        res.status(400).json({ message: "Organization not found", success: false });
+    } catch (error) {
+        res.status(400).json({ message: error, success: false });
+    }
+};
+
+export const login = async(req: Request, res: Response)=>{
+    try{
+        
+        const {username, password} = req.body.user;
+        
+        if(!username || !password){
+            res.status(400).json({ message: "Username and password are required", success: false })
+            return;
+        }
+        const user = await Users.findOne({username});
+        if(!user ){
+            res.status(200).json({ message: "usename or password is incorrect", success: false })
+            return;
+        }
+        if(user.password !== password){
+            res.status(200).json({ message: "usename or password is incorrect", success: false })
+            return;
+        }
+        const token = jwt.sign(
+            { id: user._id },
+            "your_secret_key",
+            { expiresIn: "1h" }
+        );
+        
+        res.status(200).json({token: token, user: user , message: "Login successful", success: true })
+    }
+    catch(error){
+        res.status(400).json({ message: error, success: false })
+    }
+}
+
